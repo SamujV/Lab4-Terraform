89bfc996b8ca8c943190b808e0a90e95332d0229
Samuel V <samuelviviescas@hotmail.com>
2023-10-20 22:18:34 -0500

    changes

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -3,9 +3,9 @@
 #   value       = azurerm_public_ip.ip["machine1"].ip_address
 #   sensitive   = false
 #   description = "ip Public"
-# } para seleccionar una ip en particular
+# } to select a particular IP
 
 output "ip_machines" {
-    description = "IP de los servidores"
+    description = "machine IPs"
     value = [for unique_id in azurerm_public_ip.ip : {"name": unique_id.id,"ip" : unique_id.ip_address}]
 }
